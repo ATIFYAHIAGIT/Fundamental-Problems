c7bdf382009483486d269c18f9852077514fd017
ATIFYAHIAGIT <atifyahia99@gmail.com>
2026-06-19 19:34:24 +0530

    gcd found

--- gcd.cpp
+++ gcd.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+int main () {
+    int x,y;
+    cout<<"Enter your 1st number: ";
+    cin>>x;
+    cout<<"Enter your 2nd number: ";
+    cin>>y;
+
+    for (int i=min(x,y) ; i>0; i--) {
+        if((x%i==0) && (y%i==0))   {
+            cout<<"GCD of the numbers is"<<" "<<i;
+            break;
+        }
+    }
+    return 0;
+}
