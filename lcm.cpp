689ae56a0bb94aee56f8b33518b73f3b8f79e513
ATIFYAHIAGIT <atifyahia99@gmail.com>
2026-06-19 20:42:42 +0530

    lcm done

--- lcm.cpp
+++ lcm.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std; 
 
+int main () {
+    int x,y,i=1;
+    cout<<"Enter your 1st number: ";
+    cin>>x;
+    cout<<"Enter your 2nd number: ";
+    cin>>y;
+
+    for (i; i<= (x*y);i++){
+        if ((i%x==0) && (i%y==0)) {
+            break;
+        }
+    }
+    cout<<"LCM of the input numbers is:"<<" "<<i;
+    return 0;
+}
