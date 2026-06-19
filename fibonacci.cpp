c5c8dc4b6d41de4a6c7d4695bc60c53973332fce
ATIFYAHIAGIT <atifyahia99@gmail.com>
2026-06-19 21:26:49 +0530

    fibonacci done

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+int main () {
+    int a,b,next,n;
+    cout<<"Enter the number of terms: ";
+    cin>>n;
+
+    a=0;
+    b=1;
+    next=1;
+
+    cout<<0<<" "<<1<<" ";
+    for (int i=2; i<n+1; i++) {
+        cout<<next<<" ";
+        a=b;
+        b=next;
+        next=a+b;
+   }
+    return 0;
+}
